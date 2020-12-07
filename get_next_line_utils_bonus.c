b6573db6800f23e45eb58b5d1d8c5428966caad8
sujeon <sujeon@student.42seoul.kr>
2020-12-07 19:58:57 +0900

    12-07 h/01

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sujeon <sujeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/07 17:17:56 by sujeon            #+#    #+#             */
+/*   Updated: 2020/12/06 19:19:01 by sujeon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int len;
+
+	len = 0;
+	if (!str)
+		return (0);
+	while (str[len])
+		len++;
+	return (len);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t		idx;
+	size_t		src_len;
+
+	if (!src)
+		return (0);
+	src_len = ft_strlen(src);
+	idx = 0;
+	if (!size || !src)
+		return (src_len);
+	while (idx + 1 < size && src[idx])
+	{
+		dst[idx] = src[idx];
+		idx++;
+	}
+	dst[idx] = 0;
+	return (src_len);
+}
+
+char	*ft_strdup(char *s)
+{
+	int		size;
+	int		idx;
+	char	*new_str;
+
+	idx = 0;
+	size = ft_strlen(s);
+	if (!(new_str = (char *)malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	while (idx < size)
+	{
+		new_str[idx] = s[idx];
+		idx++;
+	}
+	new_str[idx] = 0;
+	return (new_str);
+}
+
+char	*ft_strjoin(char *s1, char const *s2)
+{
+	int		s1_len;
+	int		s2_len;
+	char	*new_str;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	if (!(new_str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1))))
+		return (0);
+	ft_strlcpy(new_str, s1, s1_len + 1);
+	ft_strlcpy(new_str + s1_len, s2, s2_len + 1);
+	if (s1)
+		free_p(&s1);
+	return (new_str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char *str;
+
+	if (!s || ft_strlen(s) <= (size_t)start)
+		return (ft_strdup(""));
+	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	ft_strlcpy(str, s + start, len + 1);
+	return (str);
+}
--- main.c
+++ main.c
@@ -1,31 +1,76 @@
 #include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
 
 int main()
 {
-	int		fd;
-	char	*line;
-	int		i = 1;
-	int		n;
 	char	src[1000];
+	char	*line;
+	int		fd;
+	int		fd1;
+	int		fd2;
+	int		n;
+	int		n1;
+	int		n2;
+	int		i = 1;
+	int		j = 1;
+
 
 	//fd = 180;
-	fd = open("/home/user/gnl/42TESTERS-GNL/files/alphabet", O_RDONLY);
-	//fd = open("/home/user/gnl/test.txt", O_RDONLY);
+	//fd = open("/home/user/get_next_line/42TESTERS-GNL/files/alphabet", O_RDONLY);
+	//fd = open("/home/user/get_next_line/test.txt", O_RDONLY);
+	fd1 = open("/home/user/get_next_line/42TESTERS-GNL/files/alphabet", O_RDONLY);
+	fd2 = open("/home/user/get_next_line/42TESTERS-GNL/files/empty_lines", O_RDONLY);
 
 	//01
-	line = NULL;
-	while ((n = get_next_line(fd, &line)) != 0)
+	/*line = NULL;
+	while ((n = get_next_line(fd, &line)) > 0)
 	{
 		printf("%2d: [%d] %s\n", i, n, line);
 		free_p(&line);
 		i++;
 	}
-	printf("%2d: [%d] %s\n", i, n, line);
-	free_p(&line);
+	if (n == -1)
+		printf("error\n");
+	else
+	{
+		printf("%2d: [%d] %s\n", i, n, line);
+		free_p(&line);
+	}*/
 
 	//02
-	/*
-	while (read(fd, src, 1000))
-		;
-	printf("%s\n", src);*/
+	//line의 주솟값이 NULL
+	/*n = get_next_line(fd, NULL);
+	pr intf("n = %d\n", n);
+	printf("error\n");*/
+
+	//03
+	//여러개의 fd
+	while (n1 || n2)
+	{
+		if ((n1 = get_next_line(fd1, &line)) > 0)
+		{
+			printf("[fd1] %2d: [%d] %s\n", i, n1, line);
+			free_p(&line);
+			i++;
+		}
+		else if (!n1 && i)
+		{
+			printf("[fd1] %2d: [%d] %s\n", i, n1, line);
+			free_p(&line);
+			i = 0;
+		}
+		if ((n2 = get_next_line(fd2, &line)) > 0)
+		{
+			printf("[fd2] %2d: [%d] %s\n", j, n2, line);
+			free_p(&line);
+			j++;
+		}
+		else if (!n2 && j)
+		{
+			printf("[fd2] %2d: [%d] %s\n", j, n2, line);
+			free_p(&line);
+			j = 0;
+		}
+	}
 }
